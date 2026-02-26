dcd702187e93f8cdb801fb19857c1931506e3310
monangchoz <hamonanganjonathan7@gmail.com>
2026-02-27 00:08:45 +0700

    Add Feature to play until whoever get 5 first

--- script.js
+++ script.js
@@ -10,21 +10,48 @@ function getComputerChoice() {
     }
 }
 
-let humanChoice = prompt("rock, paper, or scissors (lowercase)")
-console.log(("You choose :") + humanChoice)
+let humanChoice;
 
-let computerChoice = getComputerChoice()
-console.log("Computer choose :" + computerChoice)
+let computerChoice;
 
 humanScore = 0;
 computerScore = 0;
 
 function playRound (humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        return "draw"
+        console.log ("Draw")
+        return 
     } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" ||humanChoice === "scissors" && computerChoice === "paper" ) {
-        return "You win!"
+        console.log ("You win!")
+        return 
     } else return "You lose!"
 }
 
-console.log(playRound(humanChoice, computerChoice))
+function playGame (humanChoice, computerChoice) {
+    while (humanScore < 5 && computerScore < 5) {
+
+        humanChoice = prompt("rock, paper, or scissors (lowercase)")
+        console.log(("You choose :") + humanChoice)
+
+        computerChoice = getComputerChoice()
+        console.log("Computer choose :" + computerChoice)
+
+        if (humanChoice === computerChoice) {
+            console.log ("Draw") 
+        } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" ||humanChoice === "scissors" && computerChoice === "paper" ) {
+            console.log ("You win!")
+            humanScore++
+        } else {
+            console.log("You lose")
+            computerScore++
+        }
+
+        console.log ("Your Score :" +humanScore)
+        console.log ("Computer Score:" + computerScore)
+        console.log ("-------------------------")
+}
+}
+
+
+
+console.log(playGame(humanChoice, computerChoice))
