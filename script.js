7fde4692c475042a563dd743a864ae80dd82094d
monangchoz <hamonanganjonathan7@gmail.com>
2026-02-26 16:46:01 +0700

    Finish able to play one round!

--- script.js
+++ script.js
@@ -10,12 +10,21 @@ function getComputerChoice() {
     }
 }
 
-let humanChoice = prompt("rock, paper, or scissors")
-console.log(humanChoice)
+let humanChoice = prompt("rock, paper, or scissors (lowercase)")
+console.log(("You choose :") + humanChoice)
 
 let computerChoice = getComputerChoice()
-console.log(computerChoice)
+console.log("Computer choose :" + computerChoice)
 
 humanScore = 0;
 computerScore = 0;
 
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "draw"
+    } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" ||humanChoice === "scissors" && computerChoice === "paper" ) {
+        return "You win!"
+    } else return "You lose!"
+}
+
+console.log(playRound(humanChoice, computerChoice))
